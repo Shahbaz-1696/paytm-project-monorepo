2bd09b7d9108f59b5bff0842d96ea0cebeba7dfb
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2024-06-25 19:37:45 +0530

    test commit

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
-// import { useBalance } from "@repo/store/balance"
+import { useBalance } from "@repo/store/balance"
+
+
+
+
 
 export default function() {
-  // const balance = useBalance();
+  const balance = useBalance();
   return <div>
-    hi there
+    hi there {balance}
   </div>
 }
